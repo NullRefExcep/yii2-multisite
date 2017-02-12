6429fde5bc97418f6d9bd9da0474ba28e6cc3571
Dmytro Karpovych <ZAYEC77@gmail.com>
2017-02-12 11:41:41 +0200

    SiteManager Init

--- src/components/SiteManager.php
+++ src/components/SiteManager.php
@@ -8,12 +8,19 @@
 
 namespace nullref\multisite\components;
 
+use Yii;
 use yii\base\Component;
 
 class SiteManager extends Component
 {
+    protected $_site = null;
+
     public function init()
     {
-        
+        $hostInfo = Yii::$app->request->getHostInfo();
+
+        if (null !== ($model = Site::find()->byHost($hostInfo)->one())) {
+            $this->_site = $model;
+        }
     }
 }
