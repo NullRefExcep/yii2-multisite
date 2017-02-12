2b4ca48558f3eaf68f3f039f57fdaacdb6510f98
Dmytro Karpovych <ZAYEC77@gmail.com>
2017-02-12 11:34:14 +0200

    fix migration

---
+++ src/components/SiteManager.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author    Dmytro Karpovych
+ * @copyright 2017 NRE
+ */
+
+
+namespace nullref\multisite\components;
+
+use yii\base\Component;
+
+class SiteManager extends Component
+{
+    public function init()
+    {
+        
+    }
+}
--- src/migrations/m170212_085035_add_site_table.php
+++ src/migrations/m170212_085035_add_site_table.php
@@ -1,6 +1,5 @@
 <?php
 
-namespace nullref\category\migrations;
 
 use nullref\core\traits\MigrationTrait;
 use yii\db\Migration;
